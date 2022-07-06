502a3f0b142460c4a2c840e560bda5b859d5a676
Agustin Palau <agu.pala@gmail.com>
2022-07-06 11:24:26 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,59 @@
+#%%
+from itertools import zip_longest
+import utils_kombat
+
+
+def get_winner_kombat(dict_kombat):
+    first_player, second_player = utils_kombat.whos_first_and_second_player(dict_kombat)
+    print(f"First player: {first_player}")
+    print(f"Second player: {second_player}")
+
+    flag = False
+
+    combinations_first_player = utils_kombat.get_combinations(dict_kombat[first_player]["movimientos"], dict_kombat[first_player]["golpes"])
+    combinations_second_player = utils_kombat.get_combinations(dict_kombat[second_player]["movimientos"], dict_kombat[second_player]["golpes"])
+
+    energia_first_player = 6
+    energia_second_player = 6
+
+    winner = ""
+
+    for comb_first, comb_second in zip_longest(combinations_first_player, combinations_second_player):
+        if comb_first:
+            danio = utils_kombat.get_damage(first_player, comb_first)
+            energia_second_player -= danio
+
+            if energia_second_player <= 0:
+                winner = first_player
+                flag = True
+                break
+
+        if comb_second:
+            danio = utils_kombat.get_damage(second_player, comb_second)
+            energia_first_player -= danio
+
+            if energia_first_player <= 0:
+                winner = second_player
+                flag = True
+                break
+
+        if flag:
+            break
+
+
+    if winner:
+        if winner == 'player1':
+            winner = 'Tonyn (player1)'
+        else: winner = 'Arnaldor (player2)'
+        print(f"Gana: {winner}")
+    else:
+        print("No hubo ganadores")
+
+
+if __name__ == "__main__":
+    try:
+        dict_kombat = {"player1":{"movimientos":["D","DSD","S","DSD","SD"],"golpes":["K","P","","K","P"]},"player2": {"movimientos":["SA","SA","SA","ASA","SA"],"golpes":["K","","K","P","P"]}}
+        get_winner_kombat(dict_kombat)
+    except Exception as e:
+        print(f'{type(e).__name__} => {e}')
+# %%
---
+++ utils_kombat.py
@@ -0,0 +1,142 @@
+#%%
+
+# General
+def longitud_lista(lista):
+    contador = 0
+    for elem in lista:
+        if elem != "":
+            contador += 1
+    return contador
+
+
+def whos_first_and_second_player(kombat_dict: dict) -> str:
+    len_player_1 = longitud_lista(kombat_dict["player1"]["movimientos"]) + longitud_lista(kombat_dict["player1"]["golpes"])
+    len_player_2 = longitud_lista(kombat_dict["player2"]["movimientos"]) + longitud_lista(kombat_dict["player2"]["golpes"])
+    
+    if len_player_1 < len_player_2:
+        return "player1", "player2"
+    elif len_player_2 < len_player_1:
+        return "player2", "player1"
+    else:
+        len_player_1 = longitud_lista(kombat_dict["player1"]["movimientos"])
+        len_player_2 = longitud_lista(kombat_dict["player2"]["movimientos"])
+        if len_player_1 < len_player_2:
+            return "player1", "player2"
+        elif len_player_2 < len_player_1:
+            return "player2", "player1"
+        else:
+            len_player_1 = longitud_lista(kombat_dict["player1"]["golpes"])
+            len_player_2 = longitud_lista(kombat_dict["player2"]["golpes"])
+            if len_player_1 < len_player_2:
+                return "player1", "player2"
+            elif len_player_2 < len_player_1:
+                return "player2", "player1"
+            else:
+                return "player1", "player2"
+
+def get_combinations(movimientos:list, golpes:list) -> list:
+    return list(zip(movimientos, golpes))
+
+
+def get_damage(player:str, combinacion: tuple) -> int:
+    if player == "player1":
+        return get_damage_from_player1(combinacion)
+    else:
+        return get_damage_from_player2(combinacion)
+
+
+#Tonyn
+def dsd_in_combination_player1(combination):
+    return 'DSD' in combination[0][-3:]
+
+def sd_in_combination_player1(combination):
+    return 'SD' in combination[0][-2:]
+
+def check_damage_taladoken_player1(combinacion):
+    dsd_in_combination = dsd_in_combination_player1(combinacion)
+    punch = 'P' in combinacion[1]
+    return dsd_in_combination and punch
+
+def check_damage_remuyuken_player1(combinacion):
+    sd_in_combination = sd_in_combination_player1(combinacion)
+    kick = 'K' in combinacion[1]
+    return sd_in_combination and kick
+
+
+def check_damage_punch_or_kick_player1(combinacion):
+    dsd_in_combination = dsd_in_combination_player1(combinacion)
+    sd_in_combination = sd_in_combination_player1(combinacion)
+    punch_in_combination = 'P' in combinacion[1]
+    kick_in_combination = 'K' in combinacion[1]
+    if not(dsd_in_combination) and not(sd_in_combination):
+        if punch_in_combination:
+            return True
+        elif kick_in_combination:
+            return True
+    else: return False
+
+def get_damage_from_player1(combinacion):
+    if check_damage_taladoken_player1(combinacion):
+        danio = 3
+        return danio
+    elif check_damage_remuyuken_player1(combinacion):
+        danio = 2
+        return danio
+    elif check_damage_punch_or_kick_player1(combinacion):
+        danio = 1
+        return danio
+    else:
+        return 0
+
+
+#Arnaldor
+
+def sa_in_combination_player2(combination):
+    return 'SA' in combination[0][-2:]
+
+def asa_in_combination_player2(combination):
+    return 'ASA' in combination[0][-3:]
+
+def check_damage_remuyuken_player2(combinacion):
+    sa_in_combination = sa_in_combination_player2(combinacion)
+    kick = 'K' in combinacion[1]
+    return sa_in_combination and kick
+
+def check_damage_taladoken_player2(combinacion):
+    asa_in_combination = asa_in_combination_player2(combinacion)
+    punch = 'P' in combinacion[1]
+    return asa_in_combination and punch
+
+def check_damage_punch_or_kick_player2(combinacion):
+    sa_in_combination = sa_in_combination_player2(combinacion)
+    asa_in_combination = asa_in_combination_player2(combinacion)
+    punch_in_combination = 'P' in combinacion[1]
+    kick_in_combination = 'K' in combinacion[1]
+    if not(sa_in_combination) and not(asa_in_combination):
+        if punch_in_combination:
+            return True
+        elif kick_in_combination:
+            return True
+    elif sa_in_combination:
+        if punch_in_combination:
+            return True
+        elif kick_in_combination:
+            return True
+    else: return False
+
+def get_damage_from_player2(combinacion):
+    danio = 0
+    if check_damage_taladoken_player2(combinacion):
+        danio = 2
+        return danio
+    elif check_damage_remuyuken_player2(combinacion):
+        danio = 3
+        return danio
+    elif check_damage_punch_or_kick_player2(combinacion):
+        danio = 1
+        return danio
+    else:
+        return danio
+
+
+# %%
